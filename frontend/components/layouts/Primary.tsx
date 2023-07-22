f5772688829fc2613408f15ce0e9b2fbdd926e45
Joy Mun <joy.js.mun@gmail.com>
2023-07-22 12:30:13 +0200

    ADD: _app.tsx

---
+++ frontend/components/layouts/Primary.tsx
@@ -0,0 +1,3 @@
+export default function Primary() {
+  return <div className="grid grid-cols-5 text-white">Primary</div>;
+}
--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,6 +1,14 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import Layout from "../components/layouts/Layout";
+
+import type { AppProps } from "next/app";
+import React from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <React.StrictMode>
+      <Layout title="MoMe">
+        <Component {...pageProps} />
+      </Layout>
+    </React.StrictMode>
+  );
 }
