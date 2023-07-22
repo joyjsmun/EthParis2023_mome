3a1b0dc0ae885b0e632236f1bbd7890961ba44d0
Joy Mun <joy.js.mun@gmail.com>
2023-07-22 13:56:10 +0200

    ADD: previous capsule and today feature

---
+++ frontend/components/Triangle.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Triangle = () => {
+  return <div className="absolute bottom-0 left-0 z-50" id="triangle"></div>;
+};
+
+export default Triangle;
---
+++ frontend/components/cm.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+const Cm = () => {
+  return (
+    <>
+      <div id="cm">
+        <div id="mm"></div>
+        <div id="mm"></div>
+        <div id="mm"></div>
+        <div id="mm"></div>
+        <div id="mm"></div>
+        <div id="mm"></div>
+        <div id="mm"></div>
+        <div id="mm"></div>
+        <div id="mm"></div>
+      </div>
+      <div id="cm">
+        <div id="mm"></div>
+        <div id="mm"></div>
+        <div id="mm"></div>
+        <div id="mm"></div>
+        <div id="mm"></div>
+        <div id="mm"></div>
+        <div id="mm"></div>
+        <div id="mm"></div>
+        <div id="mm"></div>
+      </div>
+    </>
+  );
+};
+
+export default Cm;
